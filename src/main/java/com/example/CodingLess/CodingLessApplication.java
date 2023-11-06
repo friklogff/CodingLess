aed021944eabb0a3ffdc4788114b9a10995695a0
friklogff <374591069qq.com>
2023-11-06 08:08:11 +0800

    Updated

---
+++ src/main/java/com/example/CodingLess/CodingLessApplication.java
@@ -0,0 +1,13 @@
+package com.example.CodingLess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingLessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingLessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CodingLess/CodingLessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CodingLess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingLessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
